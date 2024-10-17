8900720a69b6dc47149c8c868fb81dc448f2165c
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-10-16 22:43:33 -0300

    tailwind css

--- src/components/productComponent.ts
+++ src/components/productComponent.ts
@@ -4,13 +4,13 @@ export function showProducts(products: IProduct[]): string {
   return products
     .map(
       (product) => `
-    <div class="product">
-      <img src="${product.image}" alt="${product.title}" />
-      <h3>${product.title}</h3>
-      <p>${product.price}</p>
-      <button data-id="${product.id}">Add to Cart</button>
-    </div>
-  `
+        <div class="product">
+          <img src="${product.image}" alt="${product.title}" />
+          <h3>${product.title}</h3>
+          <p>${product.price}</p>
+          <button data-id="${product.id}">Add to Cart</button>
+        </div>
+      `
     )
     .join("");
 }
