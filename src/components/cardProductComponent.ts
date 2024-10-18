ecdfe39a8c0b66a3ad2ef8730276807b71a46fe2
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-10-18 19:28:03 -0300

    fix product

--- src/components/cardProductComponent.ts
+++ src/components/cardProductComponent.ts
@@ -8,8 +8,9 @@ export function cardProductComponent(product: IProduct): string {
   return `
     <div class="flex flex-col p-5 rounded-lg m-4 bg-ada_navyblue-50 w-80 h-96">
       <div class="flex justify-center bg-white items-center rounded-lg py-2">
-        <img src="${product.image}" alt="imagem ${product.title}"
-          class="w-32 h-32 object-contain rounded-md">
+        <img src="${product.image}" alt="imagem ${
+    product.title
+  }" class="w-32 h-32 object-contain rounded-md">
       </div>
       <h2 class="font-livvic text-white text-lg my-2">${product.title}</h2>
       <p class="font-livvic text-white text-base my-2">R$${convertPrice(
@@ -17,7 +18,7 @@ export function cardProductComponent(product: IProduct): string {
       ).toFixed(2)}</p>
       <a href="/product.html?id=${
         product.id
-      }" class="mt-auto flex justify-center py-2 px-4 items-center bg-ada_green font-livvic rounded-lg text-ada_navyblue-100">Ver Produto</a>
-      </div>
+      }" class="mt-auto flex justify-center py-2 px-4 items-center bg-ada_green font-livvic rounded-lg text-ada_navyblue-100">Adicionar ao Carrinho</a>
+    </div>
   `;
 }
