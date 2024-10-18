4ec25538d0827a6da90c6fcf9ef6413d4b50a84b
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-10-18 19:33:43 -0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/ @type {import('tailwindcss').Config} /
+/ @type {import('tailwindcss').Config} /;
 export default {
-  content: ["./public/*.html", "./src//*.{html,ts}", "./index.html"],
+  content: ["./public/*.html", "./src/**/*.{html,ts}", "./index.html"],
   theme: {
     extend: {
       fontFamily: {
